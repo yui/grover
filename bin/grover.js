e4a7beca59e7344c82c2ad1524f46670f3deaae3
Dav Glass <davglass@gmail.com>
2012-04-25 17:25:37 -0500

    Moving options to a module so it can be tested

--- bin/grover.js
+++ bin/grover.js
@@ -4,118 +4,16 @@ var path = require('path');
 var fs = require('fs');
 var exec = require('child_process').exec;
 var wrapper = path.join(__dirname, '../lib/wrapper.js');
-var args = process.argv.slice(2),
-    options = {
-        paths: [],
-        outtype: 'tap',
-        silent: false,
-        quiet: false,
-        exitOnFail: false
-    };
+var options = require(path.join(__dirname, '../lib/options')).parse(process.argv.slice(2));
 
 var START = (new Date()).getTime();
-//var VERSION = require(path.join(__dirname, '../package.json')).version;
-var VERSION = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')).version;
 var util = require(path.join(__dirname, '../lib/'));
 
-while (args.length > 0) {
-    var v = args.shift();
-    switch (v) {
-        case "-t":
-        case "--timeout":
-            var t = parseInt(args.shift());
-            if (!isNaN(t)) {
-                options.timeout = t;
-            }
-            break;
-        case "-i":
-        case "--import":
-            options.import = args.shift();
-            break;
-        case "-s":
-        case "--silent":
-            options.silent = true;
-            break;
-        case "-q":
-        case "--quiet":
-            options.quiet = true;
-            break;
-        case "-f":
-        case "--fail":
-            options.exitOnFail = true;
-            break;
-        case "-h":
-        case "--help":
-            util.log('Grover is a YUITest wrapper to run tests in PhantomJS');
-            console.log('Version ' + VERSION);
-            console.log('');
-            console.log('grover <paths to yuitest html files>');
-            console.log('   -v, --version Print version');
-            console.log('   -h, --help Print this stuff');
-            console.log('   -s, --silent Print no output, only use exit code');
-            console.log('   -q, --quiet Only print errors and use exit code');
-            console.log('   -f, --fail Fail on first error');
-            console.log('   -t, --timeout Specify a timeout (in seconds) for a test file to be considered as failed.');
-            console.log('   -i, --import <path to js file> - Require this file and use the exports (array)');
-            console.log('           as the list of files to process.');
-            console.log('   -o, --outfile <path to export file>');
-            console.log('       You can specify an export type with the following:');
-            console.log('       --tap TAP export (default)');
-            console.log('       --xml XML export');
-            console.log('       --json JSON export');
-            console.log('       --junit JUnit XML export');
-            process.exit();
-            break;
-        case "-v":
-        case "--version":
-            console.error(VERSION);
-            process.exit(1);
-            break;
-        case "-o":
-        case "--outfile":
-            options.outfile = args.shift();
-            break;
-        case "--tap":
-            options.outtype = 'TAP';
-            break;
-        case "--json":
-            options.outtype = 'JSON';
-            break;
-        case "--xml":
-            options.outtype = 'XML';
-            break;
-        case "--junit":
-            options.outtype = 'JUnitXML';
-            break;
-        default:
-            if (v.indexOf('-') === 0) {
-                console.error('Invalid option: ' + v);
-                process.exit(1);
-                break;
-            }
-            options.paths.push(v);
-    }
-}
-
-if (options.timeout <=0 ) {
-    options.timeout = null;
-}
-
-if (options.import) {
-    if (!path.existsSync(options.import) || path.existsSync(path.join(process.cwd(), options.import))) {
-        options.import = path.join(process.cwd(), options.import);
-    }
-    var paths = require(options.import);
-    if (paths && paths.length) {
-        options.paths = paths;
-    }
-}
 
 if (!options.paths.length) {
     console.error('No files given');
     process.exit(1);
 }
-
 
 var check = function(cb) {
     exec('phantomjs --version', function(stdin, stdout, stderr) {
@@ -145,26 +43,11 @@ check(function(version) {
 
 var testResults = [];
 
-var canPrint = function(json) {
-    json = json || {}
-    var p = true;
-    if (options.quiet) {
-        p = false;
-        if (json.failed) {
-            p = true;
-        }
-    }
-    if (options.silent) {
-        p = false;
-    }
-    return p;
-};
-
 var run = function() {
     var file = options.paths.shift();
     if (file) {
         if (!path.existsSync(file)) {
-            if (canPrint()) {
+            if (util.canPrint(options)) {
                 util.error(':( Can not find file: ' + file);
             }
             process.exit(1);
@@ -176,7 +59,7 @@ var run = function() {
         exec(cmd, function(err, stdout) {
             var results = JSON.parse(stdout);
             testResults.push(results);
-            if (canPrint(results)) {
+            if (util.canPrint(options, results)) {
                 util.status(results);
             }
             if (options.exitOnFail && results.failed) {
